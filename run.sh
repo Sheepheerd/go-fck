101482eb83b1fb4cb16f9f9b5a7ab21fc1cbd8b9
ReeseHatfield <hatfield.69@wright.edu>
2024-10-30 18:12:16 -0400

    project init

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit 1
+
+OUT_DIR="./bin"
+mkdir -p "$OUT_DIR"
+
+echo "building go-fck"
+go build -o "$OUT_DIR/main" ./src/cmd/main.go
+
+if [ $? -eq 0 ]; then
+    echo "go-fck  built. Running it..."
+    "$OUT_DIR/main"
+else
+    echo "Build failed."
+    exit 1
+fi
---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/Sheepheerd/go-fck
+
+go 1.23.2
